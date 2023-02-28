c3d60b7fbc02af3bc07c805c519d517a326c9427
wjsacken <wsackenheim@artscube.biz>
2023-02-28 10:41:31 -0500

    Update index.jsx

--- src/components/Navbars/ITNav/index.jsx
+++ src/components/Navbars/ITNav/index.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark style-2 position-absolute w-100 p-0" ref={navbarRef}>
       <div className="container">
         <a className="navbar-brand" href="#">
-          <img src="/assets/img/EndroW-01.svg" alt="Endro Scientific" />
+          <img src="/assets/img/Endro.svg" alt="Endro Scientific" />
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -40,38 +40,9 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav m-auto mb-2 mb-lg-0 text-uppercase">
             <li className="nav-item dropdown" onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
-              <a className="nav-link active dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Homes
+              <a className="nav-link active" href="#" id="navbarDropdown1" role="button" aria-expanded="false">
+                Home
               </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown1">
-                <li><Link to="/" className="dropdown-item">Landing Preview</Link></li>
-                <li><Link to="/home-it-solutions2" className="dropdown-item"> Creative It Solutions</Link></li>
-                <li><Link to="/home-data-analysis" className="dropdown-item"> Data Analysis</Link></li>
-                <li><Link to="/home-app-landing" className="dropdown-item"> App Landing</Link></li>
-                <li><Link to="/home-saas-technology" className="dropdown-item"> Saas Technology</Link></li>
-                <li><Link to="/home-marketing-startup" className="dropdown-item"> Marketing Startup</Link></li>
-                <li><Link to="/home-it-solutions" className="dropdown-item"> It Solution</Link></li>
-                <li><Link to="/home-software-company" className="dropdown-item"> Software Company</Link></li>
-                <li><Link to="/home-digital-agency" className="dropdown-item"> Digital Agency</Link></li>
-                <li><Link to="/home-modren-shop" className="dropdown-item"> Modren Shop</Link></li>
-              </ul>
-            </li>
-            <li className="nav-item dropdown" onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                pages
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown1">
-                <li><Link to="/page-about-5" className="dropdown-item">about</Link></li>
-                <li><Link to="/page-product-5" className="dropdown-item">product</Link></li>
-                <li><Link to="/page-services-5" className="dropdown-item">services</Link></li>
-                <li><Link to="/page-shop-5" className="dropdown-item">shop</Link></li>
-                <li><Link to="/page-single-project-5" className="dropdown-item">single project</Link></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link to="/page-portfolio-5" className="nav-link">
-                portfolio
-              </Link>
             </li>
             <li className="nav-item">
               <Link to="/page-blog-5" className="nav-link">
@@ -88,12 +59,6 @@ const Navbar = () => {
             <div className="qoute-nav ps-4">
               <a href="#" className="search-icon me-3">
                 <i className="bi bi-search"></i>
-              </a>
-              <a href="#" className="cart-icon me-3">
-                <i className="bi bi-cart"></i>
-                <span className="cart-num ms-1">
-                  2
-                </span>
               </a>
               <Link to="/page-contact-5" className="btn sm-butn border text-white radius-9 hover-darkBlue">
                 <span>Free Quote</span>
