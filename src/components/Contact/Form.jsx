060fb385815aa84ef837e016afb7f5ef1a7bd0f0
wjsacken <wsackenheim@artscube.biz>
2023-03-01 09:14:57 -0500

    Update Form.jsx

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -47,8 +47,8 @@ const Form = ({ style = "4", rtl }) => {
         style === '5' && (
           <>
             <div className="section-head text-center mb-100 style-5">
-              <h2 className="mb-20">{ rtl ? : 'Get In' } <span>{ rtl ? : 'Touch' }</span></h2>
-              <p>{ rtl ? 'We will contact again after receive your request in 24h' }</p>
+              <h2 className="mb-20">{ rtl ? 'يسعدنا' : 'Get In' } <span>{ rtl ? 'تواصلك' : 'Touch' }</span> { rtl && 'معنا' }</h2>
+              <p>{ rtl ? 'سنتواصل معك مرة أخرى بعد استلام طلبك خلال 24 ساعة' : 'We will contact again after receive your request in 24h' }</p>
             </div>
             <div className="text-center mb-100">
               <h2 className="ltspc-20 text-uppercase fs-1 lh-1 mb-50 mt-30 color-blue5">{ contactInfoData.phone }</h2>
@@ -63,52 +63,52 @@ const Form = ({ style = "4", rtl }) => {
           <div className="row justify-content-center">
             <div className="col-lg-8">
               <form action="contact.php" className="form" method="post" onSubmit={handleFormSubmit}>
-                <p className="text-center text-danger fs-12px mb-30">{ rtl ? 'The field is required mark as *' }</p>
+                <p className="text-center text-danger fs-12px mb-30">{ rtl ? 'الحقل اللذى يحتوى على هذة العلامة اجبارى *' : 'The field is required mark as *' }</p>
                 <div className="row">
                   <div className="col-lg-6">
                     <div className="form-group mb-20">
-                      <input type="text" name="name" className="form-control" placeholder={ rtl ? : "Name" } onChange={handleFormChange} />
+                      <input type="text" name="name" className="form-control" placeholder={ rtl ? 'الاسم' : "Name" } onChange={handleFormChange} />
                     </div>
                   </div>
                   <div className="col-lg-6">
                     <div className="form-group mb-20">
-                      <input type="text" name="email" className="form-control" placeholder={ rtl ? : "Email Address *" } onChange={handleFormChange} />
+                      <input type="text" name="email" className="form-control" placeholder={ rtl ? 'البريد الالكترونى *' : "Email Address *" } onChange={handleFormChange} />
                     </div>
                   </div>
                   <div className="col-lg-6">
                     <div className="form-group mb-20">
-                      <input type="text" name="phone" className="form-control" placeholder={ rtl ? : "Phone Number (option)" } onChange={handleFormChange} />
+                      <input type="text" name="phone" className="form-control" placeholder={ rtl ? 'رقم الهاتف (اختياري)' : "Phone Number (option)" } onChange={handleFormChange} />
                     </div>
                   </div>
                   <div className="col-lg-6">
                     <div className="form-group mb-20">
-                      <input type="text" name="website" className="form-control" placeholder={ rtl ? : "Your Website (option)" } onChange={handleFormChange} />
+                      <input type="text" name="website" className="form-control" placeholder={ rtl ? 'رابط موقعك (اختيارى)' : "Your Website (option)" } onChange={handleFormChange} />
                     </div>
                   </div>
                   <div className="col-lg-12">
                     <div className="form-group mb-20">
-                      <select className="form-select" defaultValue={ rtl ? : "How can we help you?" } name="option" onChange={handleFormChange}>
-                        <option value="how can we help">{ rtl ?   : 'How can we help you?' }</option>
-                        <option value="option 1">{ rtl ?  : 'option 1' }</option>
-                        <option value="option 2">{ rtl ?   : 'option 2' }</option>
+                      <select className="form-select" defaultValue={ rtl ? 'كيف يمكننا مساعدتك ؟' : "How can we help you?" } name="option" onChange={handleFormChange}>
+                        <option value="how can we help">{ rtl ? 'كيف يمكننا مساعدتك ؟' : 'How can we help you?' }</option>
+                        <option value="option 1">{ rtl ? 'الاختيار الاول' : 'option 1' }</option>
+                        <option value="option 2">{ rtl ? 'الاختيار الثاني' : 'option 2' }</option>
                       </select>
                     </div>
                   </div>
                   <div className="col-lg-12">
                     <div className="form-group">
-                      <textarea rows="10" name="message" className="form-control" placeholder={ rtl ? : "How can we help you?" } onChange={handleFormChange}></textarea>
+                      <textarea rows="10" name="message" className="form-control" placeholder={ rtl ? 'كيف يمكننا مساعدتك ؟' : "How can we help you?" } onChange={handleFormChange}></textarea>
                     </div>
                   </div>
                   <div className="col-lg-12 text-center">
                     <div className="form-check d-inline-flex mt-30 mb-30">
                       <input className="form-check-input me-2 mt-0" type="checkbox" value="" id="flexCheckDefault" />
                       <label className="form-check-label small" htmlFor="flexCheckDefault">
-                        { rtl ? : 'By submitting, i’m agreed to the' } <a href="#" className="text-decoration-underline">{ rtl ? : 'Terms & Conditons' }</a>
+                        { rtl ? 'من خلال الإرسال ، أوافق على' : 'By submitting, i’m agreed to the' } <a href="#" className="text-decoration-underline">{ rtl ? 'الشروط و الاحكام' : 'Terms & Conditons' }</a>
                       </label>
                     </div>
                   </div>
                   <div className="col-lg-12 text-center">
-                    <input type="submit" value={ rtl ? : 'Send Your Request' } className="btn rounded-pill blue5-3Dbutn hover-blue2 sm-butn fw-bold text-light" />
+                    <input type="submit" value={ rtl ? 'ارسل طلبك' : 'Send Your Request' } className="btn rounded-pill blue5-3Dbutn hover-blue2 sm-butn fw-bold text-light" />
                   </div>
                 </div>
               </form>
