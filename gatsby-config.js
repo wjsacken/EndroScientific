ee37bdc565178f0e93f977d85a4f0a827a6ddcd6
wjsacken <wsackenheim@artscube.biz>
2023-09-19 08:55:26 -0400

    env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,27 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: 'Iteck',
     description: 'Iteck - Multi-Purpose React.js Gatsby Template',
     author: 'ThemesCamp'
   },
+
   plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-eslint',
+    `gatsby-plugin-resolve-src`,
+    `gatsby-plugin-eslint`,
     {
-      resolve: "gatsby-source-shopify",
+      resolve: `gatsby-source-shopify`,
       options: {
-        storeUrl: "7cdf1c.myshopify.com",
-        password: "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",
-        salesChannel: "82998a8b-c8e9-4dab-92d3-61defe2a148a",
-        apiVersion: "2023-04",
-        shopifyConnections: ['collections']
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+        password: process.env.GATSBY_SHOPIFY_PASSWORD,
+        salesChannel: process.env.SALES_CHANNEL,
+        apiVersion: process.env.API_VERSION,
+        shopifyConnections: [`collections`]
       },
     },
-    "gatsby-plugin-image"
+    `gatsby-plugin-image`
   ],
-  trailingSlash: "always"
+  trailingSlash: `always`
 }
